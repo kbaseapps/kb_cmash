a34944ee546260850b6c8286a0b319401a082bd9
slebras <slebras@Sebastians-MacBook-Pro.local>
2019-04-04 09:31:40 -0700

    ui and genomeset download updates

--- lib/kb_cmash/utils/misc_utils.py
+++ lib/kb_cmash/utils/misc_utils.py
@@ -1,14 +1,13 @@
 import os
 from installed_clients.AssemblyUtilClient import AssemblyUtil
 from installed_clients.DataFileUtilClient import DataFileUtil
-from installed_clients.GenomeFileUtilClient import GenomeFileUtil
 
 def load_fastas(callback_url, scratch, upa):
     '''
 
     '''
     dfu = DataFileUtil(callback_url)
-
+    au = AssemblyUtil(callback_url)
     obj_data = dfu.get_objects({"object_refs":[upa]})['data'][0]
     obj_type  = obj_data['info'][2]
 
@@ -20,15 +19,16 @@ def load_fastas(callback_url, scratch, upa):
         upas = [upa]
     elif "KBaseGenomes.ContigSet" in obj_type or "KBaseGenomeAnnotations.Assembly" in obj_type:
         # in this case we use the assembly file util to get the fasta file
-        au = AssemblyUtil(callback_url)
         file_output = os.path.join(scratch, "input_fasta.fa")
         faf = au.get_assembly_as_fasta({"ref": upa})
-        fasta_path = faf['path']
-        return [(fasta_path, upa)]
+        return [(faf['path'], upa)]
 
-    gfu = GenomeFileUtil(callback_url)
+    data_objs = dfu.get_objects({"object_refs":upas})['data']
+    assembly_upas = [d['data']['assembly_ref'] for d in data_objs]
     fasta_paths = []
-    for upa in upas:
-        faf = gfu.genome_features_to_fasta({"genome_ref": upa})
-        fasta_paths.append((faf['file_path'], upa))
+    for upa in assembly_upas:
+        # first we want to ge the associated assemblies.
+        faf = au.get_assembly_as_fasta({"ref":upa})
+        # faf = gfu.genome_features_to_fasta({"genome_ref": upa})
+        fasta_paths.append((faf['path'], upa))
     return fasta_paths
